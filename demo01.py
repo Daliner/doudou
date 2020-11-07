3580fb3269b5e3341dd9d6d263ed064ad2a6fc85
doudou <1602644186@qq.com>
2020-11-07 13:29:08 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,32 @@
+"""
+print("你好呀，python!")
+print(23333)  # 整数
+print(True) #布尔
+print(())  # 小括号
+print({})
+print([])
+
+天上的星星不说话
+
+print("哈哈哈"+"哈哈")
+print("哈哈哈"*100)
+print(((1+2)*100-99)/2)
+print(2>3)
+"""
+
+
+###print("input 获取的内容是：",a+b)
+#数据格式的转换
+"""
+print(type(2333))
+print(type("只能杂散"))
+print(type(()))
+print(type([]))
+print(type({}))
+"""
+#a = float(input("请输入： "))
+#b = float(input("请输入： "))
+#print("input获取的内容是：",a+b)
+
+m = "ffffff"
+print(len(m))
